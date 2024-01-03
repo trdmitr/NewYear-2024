f25ff6f01dac349fa21cf747d073311079525d7b
trdmitr <trdmitr@gmail.com>
2024-01-03 18:22:57 +0400

    Updates

---
+++ precache-manifest.32d1470cbbbd4758d9d60606fbcadc89.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ac6a00ab4816548fa60",
+    "url": "/NewYear-2024/static/css/main.88f1cedc.chunk.css"
+  },
+  {
+    "revision": "4ac6a00ab4816548fa60",
+    "url": "/NewYear-2024/static/js/main.09d53baf.chunk.js"
+  },
+  {
+    "revision": "209d14cc6cd16715e91b",
+    "url": "/NewYear-2024/static/js/runtime~main.e4e58dd6.js"
+  },
+  {
+    "revision": "082d59b04e082ef59cc6",
+    "url": "/NewYear-2024/static/css/2.e9e2c4e1.chunk.css"
+  },
+  {
+    "revision": "082d59b04e082ef59cc6",
+    "url": "/NewYear-2024/static/js/2.46900b6b.chunk.js"
+  },
+  {
+    "revision": "f357f7882a4bd5bcec5f",
+    "url": "/NewYear-2024/static/js/3.37352ce7.chunk.js"
+  },
+  {
+    "revision": "e0df83dfd120abdfc7a63460862e9f8d",
+    "url": "/NewYear-2024/static/media/Peter_Ivanovitsch.e0df83df.ttf"
+  },
+  {
+    "revision": "4540b114cf97488772c4c9f8a60f585c",
+    "url": "/NewYear-2024/index.html"
+  }
+];
